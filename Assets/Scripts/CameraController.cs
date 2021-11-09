f60d9e552a9c3a52a0523983085aa41a92647521
Johnny Howe <jonathon.o.howe@gmail.com>
2021-11-10 09:21:31 +1300

    Smoother camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,13 +6,18 @@ public class CameraController : MonoBehaviour
 {
     public Vector2 offset;
     public Transform followPoint;
+    public float speed = 1f;
+    Vector3 velocity = Vector3.zero;
 
     void LateUpdate()
     {
-        transform.position = new Vector3(
+        Vector3 goal = new Vector3(
             followPoint.position.x,
             followPoint.position.y,
             transform.position.z
         ) + (Vector3) offset;
+        float distance = (goal - transform.position).magnitude;
+        float smoothTime = speed / distance;
+        transform.position = Vector3.SmoothDamp(transform.position, goal, ref velocity, smoothTime);
     }
 }
